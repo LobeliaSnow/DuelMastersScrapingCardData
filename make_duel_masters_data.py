e8fc8169544bf619ea43cee434feca0054896a62
雪瑠璃 <lobelia.snow@gmail.com>
2020-08-08 17:17:58 +0900

    全要素引っ張ってくるように

--- make_duel_masters_data.py
+++ make_duel_masters_data.py
@@ -25,26 +25,82 @@ def LoadEnviormentVariables(enviorment_path):
             chrome_driver_path = f.readlines()[0].rstrip('\n')
 
 class DuelMastersCard:
-    def __init__(self, card_page):
-        self.card_page = card_page
+    def __init__(self, card_page, link):
+        self.site_link = link
         # 以下能力取得
-        self.name = self.card_page.select_one('#mainContent > section > table > tbody > tr.windowtitle > th').text
-        self.type = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(2) > td.typetxt').text
-        self.civilization = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(2) > td.civtxt').text
-        self.rarity = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(3) > td.raretxt').text
-        self.power = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(3) > td.powertxt').text
-        self.cost = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(4) > td.costtxt').text
-        self.race = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(5) > td').text
-        ability_raw = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-child(7) > td')
+        name_data = card_page.select_one('#mainContent > section > table > tbody > tr.windowtitle > th').text
+        index = name_data.rfind('(')
+        self.name , self.pack = name_data[:index], name_data[index:]
+        self.type = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(2) > td.typetxt').text
+        self.civilization = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(2) > td.civtxt').text
+        self.rarity = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(3) > td.raretxt').text
+        self.power = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(3) > td.powertxt').text
+        self.cost = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(4) > td.costtxt').text.split(' ')[0]
+        self.race = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(5) > td').text
+        ability_raw = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(7) > td')
         self.ability = ''
         for ability in ability_raw:
-            self.ability += ability.text + '\n'
-        self.flavor = self.card_page.select_one('#mainContent > section > table > tbody > tr:nth-child(9) > td').text
+            self.ability += ability.text.replace('\n', '') + '\n'
+        self.ability = self.ability.rstrip('\n')
+        self.flavor = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(9) > td').text.replace('\n', '')
+        self.pic_url = card_page.select_one('#mainContent > section > table > tbody > tr:nth-child(2) > td.cardarea > div > img').get('src')
+        # 以下ツインパクト、ドラグハート、サイキック用
+        name_data = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr.windowtitle > th')
+        if name_data is None:
+            return
+        index = name_data.text.rfind('(')
+        self.second_name = name_data.text[:index]
+        self.second_type = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(2) > td.typetxt').text
+        self.second_civilization = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(2) > td.civtxt').text
+        self.second_rarity = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(3) > td.raretxt').text
+        self.second_power = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(3) > td.powertxt').text
+        self.second_cost = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(4) > td.costtxt').text.split(' ')[0]
+        self.second_race = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(5) > td').text
+        ability_raw = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(7) > td')
+        self.ability = ''
+        for ability in ability_raw:
+            self.ability += ability.text.replace('\n', '') + '\n'
+        self.second_ability = self.ability.rstrip('\n')
+        self.second_flavor = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-of-type(9) > td').text.replace('\n', '')
+        self.second_pic_url = card_page.select_one('#mainContent > section > table:nth-of-type(2) > tbody > tr:nth-child(2) > td.cardarea > div > img').get('src')
+        # 3D龍解用
+        name_data = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr.windowtitle > th')
+        if name_data is None:
+            return
+        index = name_data.text.rfind('(')
+        self.third_name = name_data.text[:index]
+        self.third_type = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(2) > td.typetxt').text
+        self.third_civilization = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(2) > td.civtxt').text
+        self.third_rarity = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(3) > td.raretxt').text
+        self.third_power = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(3) > td.powertxt').text
+        self.third_cost = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(4) > td.costtxt').text.split(' ')[0]
+        self.third_race = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(5) > td').text
+        ability_raw = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(7) > td')
+        self.ability = ''
+        for ability in ability_raw:
+            self.ability += ability.text.replace('\n', '') + '\n'
+        self.third_ability = self.ability.rstrip('\n')
+        self.third_flavor = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-of-type(9) > td').text.replace('\n', '')
+        self.third_pic_url = card_page.select_one('#mainContent > section > table:nth-of-type(6) > tbody > tr:nth-child(2) > td.cardarea > div > img').get('src')
 
     # printで表示するためのもの、デバッグ用
     def __str__(self):
-        return 'name : ' + self.name + '\n' + 'type : ' + self.type + '\n' + 'civilization : ' + self.civilization+ '\n' + 'rarity : ' + self.rarity + '\n' \
+        first_card = 'name : ' + self.name + '\n' + 'pack : ' + self.pack + '\n' + 'type : ' + self.type + '\n' + 'civilization : ' + self.civilization+ '\n' + 'rarity : ' + self.rarity + '\n' \
         + 'power : ' + self.power + '\n' + 'cost : ' + self.cost + '\n' + 'race : ' + self.race + '\n' + 'ability : ' + self.ability + '\n' + 'flavor : ' + self.flavor + '\n'
+        ret_value =  'first card : \n' + first_card
+        try:
+            second_card = 'name : ' + self.second_name + '\n' + 'type : ' + self.second_type + '\n' + 'civilization : ' + self.second_civilization+ '\n' + 'rarity : ' + self.second_rarity + '\n' \
+            + 'power : ' + self.second_power + '\n' + 'cost : ' + self.second_cost + '\n' + 'race : ' + self.second_race + '\n' + 'ability : ' + self.second_ability + '\n' + 'flavor : ' + self.second_flavor + '\n'
+        except:
+            return ret_value
+        ret_value += '\n' + second_card
+        try:
+            third_card = 'name : ' + self.third_name + '\n' + 'type : ' + self.third_type + '\n' + 'civilization : ' + self.third_civilization+ '\n' + 'rarity : ' + self.third_rarity + '\n' \
+            + 'power : ' + self.third_power + '\n' + 'cost : ' + self.third_cost + '\n' + 'race : ' + self.third_race + '\n' + 'ability : ' + self.third_ability + '\n' + 'flavor : ' + self.third_flavor + '\n'
+        except:
+            return ret_value
+        ret_value += '\n' + third_card
+        return ret_value
 
 class DuelMastersCardBox:
     def __init__(self, driver):
@@ -74,15 +130,13 @@ class DuelMastersCardBox:
                 element_driver = connect_html.GetDriver('https://dm.takaratomy.co.jp' + data_link, chrome_driver_path, headless_mode)
                 card_page = connect_html.GetBeautifulSoupFromDriver(element_driver)
                 # カード情報をスクレイピング
-                card = DuelMastersCard(card_page)
+                card = DuelMastersCard(card_page, data_link)
                 # カードページを開いているchromeを閉じる
                 connect_html.ReleaseDriver(element_driver)
                 # デバッグ表示、気になる人はつけるとよい
                 print(card)
                 # カードボックスへプール
                 self.card_box.append(card)
-                break
-            break
 
 if __name__ == '__main__':
     LoadEnviormentVariables("enviorment.txt")
