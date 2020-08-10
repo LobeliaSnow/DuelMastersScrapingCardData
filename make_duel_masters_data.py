9a150f998ea7f389af8194907a80f9ac49676d84
雪瑠璃 <lobelia.snow@gmail.com>
2020-08-10 17:35:26 +0900

    復帰機能作成

--- make_duel_masters_data.py
+++ make_duel_masters_data.py
@@ -15,14 +15,16 @@ import os
 import csv
 import platform
 import concurrent.futures
+import time
 
 # 環境変数
 # enviorment.iniで変えるようにしてください
 chrome_driver_path = 'chromedriver'
 headless_mode = True
 master_path = 'master.csv'
+thread_count = -1
 
-# TODO マルチスレッド
+# TODO 復帰機能
 
 
 def SplitList(list, n):
@@ -36,6 +38,7 @@ def LoadEnviormentVariables(enviorment_path):
     global chrome_driver_path
     global headless_mode
     global master_path
+    global thread_count
     # 環境変数用ファイルの存在を確認
     if os.path.exists(enviorment_path):
         config = configparser.ConfigParser()
@@ -53,6 +56,11 @@ def LoadEnviormentVariables(enviorment_path):
         try:
             data = config.get("settings", "master_path")
             master_path = data
+        except:
+            pass
+        try:
+            data = config.get("settings", "thread_count")
+            thread_count = int(data)
         except:
             pass
 
@@ -74,7 +82,11 @@ class DuelMastersCard:
         ability_raw = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(7) > td')
         self.ability = ''
         for ability in ability_raw:
-            self.ability += ability.text.replace('\n', '') + '\n'
+            # 途中で改行を挟んでくる奴が発見されて、推定そこで例外を吐いたのでその対応
+            try:
+                self.ability += ability.text.replace('\n', '') + '\n'
+            except:
+                pass
         self.ability = self.ability.rstrip('\n')
         self.flavor = card_page.select_one('#mainContent > section > table > tbody > tr:nth-of-type(9) > td').text.replace('\n', '')
         self.pic_url = card_page.select_one('#mainContent > section > table > tbody > tr:nth-child(2) > td.cardarea > div > img').get('src')
@@ -139,6 +151,7 @@ class DuelMastersCard:
         ret_value += '\n' + third_card
         return ret_value
 
+# 各ページのカードリストを処理するための関数
 def CardPageProcedure(card_list):
     page_driver = None
     temp_box = []
@@ -165,15 +178,65 @@ def CardPageProcedure(card_list):
 
 # カードボックス
 class DuelMastersCardBox:
-    def __init__(self, driver, write_file):
-        self.writer = csv.writer(write_file)
-        self.WriteCardBoxHeader()
+    def __init__(self, driver, start_index, writer):
+        # csvの書き込み用
+        self.writer = csv.writer(writer)
+        # 以下スクレイピング用
         self.driver = driver
         html = connect_html.GetBeautifulSoupFromDriver(self.driver)
         self.page_count = html.select_one('#cardlist > div > div > a.nextpostslink').get('data-page')
         self.card_access_list = []
         self.card_box = []
-        for page in range(1, int(self.page_count)):
+        # 何ページ目から検索するか
+        start_page = 1
+        start_card = 1
+        # 既存のCSVが存在している場合
+        if start_index > 1:
+            # デュエマに存在するカード枚数を取得
+            card_total_count = html.select_one('#total_count').text
+            # すでに最大枚数取得している場合は新規カードはないので終了
+            if start_index == int(card_total_count):
+                return
+            # 復帰する必要が出てきたので、1ページに存在する最大数を取得
+            page_card_max_count = len(html.select("#cardlist > ul > li"))
+            # ページの復帰場所
+            start_page = int(start_index / page_card_max_count) + 1
+            # カードの復帰場所
+            start_card = int(start_index % page_card_max_count)
+        else:
+            # ヘッダー作成
+            self.WriteCardBoxHeader()
+        global thread_count
+        # 0以下ならCPUにとって最適なコア数に
+        if thread_count < 0:
+            # CPUの論理コア数取得
+            thread_count = psutil.cpu_count()
+
+        # ページの復帰処理
+        if start_page > 1 and start_page != 2:
+            # TODO 最適化
+            # start_page-1の理由は一発目の処理時に次のページへ進むため
+            for recover_index in range(2, start_page):
+                if recover_index == start_page:
+                    recover_index = start_page - 1
+                # 2ページずつ飛ばす
+                elif recover_index % 2 == 0 and recover_index != start_page:
+                    continue
+                try:
+                    link = self.driver.find_element_by_link_text(str(recover_index))
+                except:
+                    #リンクが運悪く取得できなかった場合は一旦1秒待つ
+                    time.sleep(1)
+                    link = self.driver.find_element_by_link_text(str(recover_index))
+                driver.execute_script("arguments[0].click();", link)              
+                # 以下ajaxの読み込み完了待ち
+                wait = WebDriverWait(driver, 15)
+                wait.until(lambda driver: driver.execute_script('return jQuery.active') == 0)
+                wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+        # ここで一旦待ち、ページのロードが終わっていない可能性
+        time.sleep(1)
+        # 各ページ用
+        for page in range(start_page, int(self.page_count)):
             # 次のページへジャンプ
             if int(page) > 1:
                 link = self.driver.find_element_by_link_text(str(page))
@@ -184,19 +247,30 @@ class DuelMastersCardBox:
                 wait.until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
                 # 更新されたページのhtmlを取得する
                 html = connect_html.GetBeautifulSoupFromDriver(self.driver)
-            # カードリストを順次探索
+            # カードリスト取得
             card_list = html.select("#cardlist > ul > li")
+            # カードの復帰処理
+            if start_card > 0:
+                del card_list[0 : start_card - 1]
+                start_card = 0
             self.page_card_box = []
-            cpu_logical_core_count = psutil.cpu_count()
-            split_card_list = SplitList(card_list, cpu_logical_core_count)
-            with concurrent.futures.ThreadPoolExecutor(max_workers = cpu_logical_core_count) as executor:
-                results = list(executor.map(CardPageProcedure, split_card_list))
-            for result in results:
-                self.page_card_box.extend(result)
+            # マルチスレッドモード
+            if thread_count > 0:
+                # スレッド数分に分割
+                split_card_list = SplitList(card_list, thread_count)
+                # マルチスレッドによるカードデータの収集
+                with concurrent.futures.ThreadPoolExecutor(max_workers = thread_count) as executor:
+                    results = list(executor.map(CardPageProcedure, split_card_list))
+                # 各スレッドの結果を結合
+                for result in results:
+                    self.page_card_box.extend(result)
+            else:
+                # シングルスレッドモード
+                self.page_card_box = CardPageProcedure(card_list)
             # csv書き出し
-            if write_file is not None:
+            if writer is not None:
                 self.WriteCardBoxCSV(self.page_card_box)
-
+            break
     # 以下csv書き出し用関数
     def WriteCardBoxHeader(self):
         if self.writer is None:
@@ -230,9 +304,19 @@ if __name__ == '__main__':
     LoadEnviormentVariables("enviorment.ini")
     # chrome起動、操作権取得
     driver = connect_html.GetDriver('https://dm.takaratomy.co.jp/card/', chrome_driver_path, headless_mode)
-    with open(master_path, 'w', newline="", encoding='utf-8') as file:
+    # 検索開始するカードの位置、既存のカード枚数 + 1
+    row_count = 1
+    # 既存のファイルが存在するのであれば
+    if os.path.exists(master_path):
+        with open(master_path, 'r', newline="", encoding='utf-8') as file:
+            reader = csv.reader(file)
+            # ヘッダーが含まれて実際のカード枚数より1多いが、最後のカードの一つ先から読み込むので問題なし
+            row_count = len(list(reader))
+        merge = True
+    with open(master_path, 'a', newline="", encoding='utf-8') as file:
         # カードボックス取得
-        cardbox = DuelMastersCardBox(driver, file)
+        cardbox = DuelMastersCardBox(driver, row_count, file)
+    
     # chromeの解放 これをしないとバックグラウンドプロセスにchrome driverとgoogle chromeが大量に発生する
     connect_html.ReleaseDriver(driver)
     
